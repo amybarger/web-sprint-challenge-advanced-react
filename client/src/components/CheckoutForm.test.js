d2afdd20d46b2a13e6bd74da1808c54d08b02d25
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-06-14 20:28:46 -0400

    Creating tests

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,16 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+    breakTheTest();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    breakTheTest();
+});
+
+function breakTheTest() {
+    throw new Error("This is an intentional fail of the test")
+}
